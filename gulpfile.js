e0a645ef97a7b798a01b4d8e991a75c4a52ced44
LanzaDelRelampago <novenosexta77@gmail.com>
2023-11-10 08:25:34 +0000

    build

--- gulpfile.js
+++ gulpfile.js
@@ -14,10 +14,8 @@ const comments = require("gulp-header-comment");
 const template = require("gulp-template");
 const theme = require("./src/theme.json");
 const node_env = process.argv.slice(2)[0];
-const headerComments = `WEBSITE: https://themefisher.com
-                        TWITTER: https://twitter.com/themefisher
-                        FACEBOOK: https://facebook.com/themefisher
-                        GITHUB: https://github.com/themefisher/`;
+const headerComments = `WEBSITE: https://bluetech-tg.github.io/
+                        GITHUB: https://github.com/Bluetech-TG`;
 
 var path = {
   // source paths
